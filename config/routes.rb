6934aeb8f95e11bfbb2b2da16e79c1f4d54beef8
adifsgaid <infosgaid@gmail.com>
2020-11-24 17:22:36 +0100

    Created dashboard and qlinks

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   resources :todo_lists, only:[:index, :update]
 
   resources :users, only:[:edit, :update]
+  get '/dashboard' => 'users#dashboard', as: :dashboard
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
