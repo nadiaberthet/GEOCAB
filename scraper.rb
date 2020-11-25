2c49870e9ec16e4579d7bb5a9f52683cd15eaf95
adifsgaid <infosgaid@gmail.com>
2020-11-25 12:23:26 +0100

    &

--- scraper.rb
+++ scraper.rb
@@ -1,21 +1,15 @@
-require 'httparty'
 require 'nokogiri'
 require 'byebug'
-require 'json'
 require 'open-uri'
 
-#   ingredient =
-#   url = "https://www.bbcgoodfood.com/search/recipes?q=#{ingredient}"
-
-#   html_file = open(url).read
-#   html_doc = Nokogiri::HTML(html_file)
-
-#   html_doc.search('.standard-card-new__article-title').each do |element|
-#     puts element.text.strip
-#     puts element.attribute('href').value
-# end
-
 def all_ads
-  unparsed_page = HTTParty.get('https://www.worldometers.info/coronavirus/')
-  parsed_page = Nokogiri::HTML(unparsed_page)
+  url = 'http://www.annoncesparamedicales.com/jsp/annoncesParVille_lyon.html'
+
+  html_file = open(url).read
+  html_doc  = Nokogiri::HTML(html_file)
+
+  html_doc.search('.annonce_search_container"').each do |element|
+    puts element.text
+    puts element.attribute('href').value
+  end
 end
