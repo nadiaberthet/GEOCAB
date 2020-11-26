bbc64103c0d5d6067f3c413f2da6247c1fd86a07
adifsgaid <infosgaid@gmail.com>
2020-11-26 16:36:07 +0100

    added image to map

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -15,10 +15,10 @@ class PagesController < ApplicationController
     @markers = @ads.map do |ad|
       {
         lat: ad.latitude,
-        lng: ad.longitude
-        # infoWindow: render_to_string(partial: "info_window", locals: { ad: ad }),
+        lng: ad.longitude,
+        infoWindow: render_to_string(partial: "info_window", locals: { ad: ad }),
         # image_url: helpers.asset_url("image"),
-        # id: ad.id
+        id: ad.id
       }
     end
   end
--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -32,4 +32,16 @@ const initMapbox = () => {
   }
 };
 
+    // const markers = JSON.parse(mapElement.dataset.markers);
+    // markers.forEach((marker) => {
+    //   const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
+
+    //   new mapboxgl.Marker()
+    //     .setLngLat([ marker.lng, marker.lat ])
+    //     .setPopup(popup)
+    //     .addTo(map);
+    // });
+    // fitMapToMarkers(map, markers);
+
+
 export { initMapbox };
