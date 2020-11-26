538fbf9c64b9595ea179b92df892807486714370
Zach Ross <zachariahrossy@gmail.com>
2020-11-26 15:09:11 +0100

    scrunk

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,16 +5,12 @@ class PagesController < ApplicationController
     @user = current_user
     @search = Search.new
   end
-
   def insights
     @user = current_user
     @search = Search.find(params[:id])
-    if !@search.nil?
-      @ad = Ad.near([@search.latitude, @search.longitude], 10)
-    else
-      @ads = Ad.all
-    end
-    # the `geocoded` scope filters only flats with coordinates (latitude & longitude)
+    @ad = Ad.find(3).address
+    @user_search = @ad.([@search.latitude, @search.longitude], 10)
+    # the geocoded scope filters only flats with coordinates (latitude & longitude)
     @markers = @ads.map do |ad|
       {
         lat: ad.latitude,
--- app/models/ad.rb
+++ app/models/ad.rb
@@ -1,3 +1,6 @@
 class Ad < ApplicationRecord
   validates :address, format: { with: /\S\S\W*((?i)Arrondissement(?-i))\W*/ }
+  after_validation :geocode
+
+  geocoded_by :address
 end
