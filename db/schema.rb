5b46a93d2c7a11cfe70548eeac71b19d44d91970
Nadia <nadia.bachi@gmail.com>
2020-11-26 17:30:42 +0100

    schema update

--- db/schema.rb
+++ db/schema.rb
@@ -77,6 +77,7 @@ ActiveRecord::Schema.define(version: 2020_11_26_131145) do
     t.boolean "assurance_rcp", default: false
     t.integer "budget"
     t.string "commodite"
+    t.string "job"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
