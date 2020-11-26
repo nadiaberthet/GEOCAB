deb1a28713bdd67b6d02d73186742995af7d8720
Zach Ross <zachariahrossy@gmail.com>
2020-11-26 10:47:07 +0100

    dunno what's on here

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -30,10 +30,5 @@ import { initMapbox } from '../plugins/init_mapbox';
 // import { initSelect2 } from '../components/init_select2';
 
 document.addEventListener('turbolinks:load', () => {
-  // Call your functions here, e.g:
-  // initSelect2();
-});
-
-document.addEventListener('turbolinks:load', () => {
   initMapbox();
 })
