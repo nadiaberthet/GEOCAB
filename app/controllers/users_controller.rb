2b16bed74b9dfe49c7c352d2fda48c479df04355
Nadia <nadia.bachi@gmail.com>
2020-11-27 11:57:48 +0100

    user_controller update - to check

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,15 @@ class UsersController < ApplicationController
   end
 
   def update
+    current_user.update(user_params)
   end
 
   def dashboard
+  end
+
+  private
+
+  def user_params
+    params.require(:users).permit(:job)
   end
 end
