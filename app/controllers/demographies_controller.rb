62d45cbf529312c1ae7547d1b1ca88e84ac09a92
adifsgaid <infosgaid@gmail.com>
2020-11-30 14:18:59 +0100

    adde demo

---
+++ app/controllers/demographies_controller.rb
@@ -0,0 +1,55 @@
+class DemographiesController < ApplicationController
+  require 'google-places'
+  before_action :set_place, only: [:show, :edit, :update]
+
+  def index
+    @places = Place.all
+  end
+
+  def nearbies
+    @client = GooglePlaces::Client.new(ENV["PLACES_API_KEY"])
+    @places = @client.spots(@ads)
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def create
+    @place = Place.new(place_params)
+
+    respond_to do |format|
+      if @place.save
+        format.html { redirect_to @place, notice: 'Place was successfully created.' }
+        format.json { render :show, status: :created, location: @place }
+      else
+        format.html { render :new }
+        format.json { render json: @place.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @place.update(place_params)
+        format.html { redirect_to @place, notice: 'Place was successfully updated.' }
+        format.json { render :show, status: :ok, location: @place }
+      else
+        format.html { render :edit }
+        format.json { render json: @place.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def set_place
+    @place = Place.find(params[:id])
+  end
+
+  def place_params
+    params.require(:place).permit(:title, :address, :latitude, :longitude)
+  end
+end
