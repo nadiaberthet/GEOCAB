7773b66c7e4f4af67158ecf583177587f3f484d5
Zach Ross <zachariahrossy@gmail.com>
2020-11-30 12:48:28 +0100

    trying to fix this push

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  get 'mes_locaux', to: 'users#mes_locaux'
   get 'dashboard', to: 'searches#dashboard', as: :dashboard
   get '/users/:id/mes_locaux', to: 'users#mes_locaux', as: "mes_locaux"
   get '/users/:id/chiffres_cles', to: 'users#chiffres_cles', as: "chiffres_cles"
