9f7900671d2b2ea29dbc606e2e7d6e776a31e99d
adifsgaid <infosgaid@gmail.com>
2021-01-25 11:01:08 +0100

    predispo API

--- app/services/api_insee.rb
+++ app/services/api_insee.rb
@@ -1,2 +1,10 @@
+require 'open-uri'
+require 'json'
 class ApiInsee
+  def donne_demo(ville)
+    url = 'https://api.insee.fr/donnees-locales/V0.1/donnees/geo-TF4@RP2015/COM-44109.all'
+    demo_serialized = open(url).read
+    ville = JSON.parse(demo_serialized)
+  end
+  donne_demo(puts "#{demo_serialized} - #{ville}")
 end
